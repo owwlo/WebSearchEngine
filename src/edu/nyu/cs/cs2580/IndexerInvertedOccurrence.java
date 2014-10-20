6f493de37d33aabd9cd9087fa8e2133da27134fa
hui-yang <hy821@nyu.edu>
2014-10-20 15:54:39 -0400

    linear search

--- src/edu/nyu/cs/cs2580/IndexerInvertedOccurrence.java
+++ src/edu/nyu/cs/cs2580/IndexerInvertedOccurrence.java
@@ -510,6 +510,22 @@ public class IndexerInvertedOccurrence extends Indexer {
         return bsInner(0, list.size() - 1, docId, list);
     }
 
+    // do linear search of a docid for compressed posting list, first occurrence
+    private int linearSearchPostList(final int docId, final List<Integer> list) {
+        int i = 0;
+        int pos = -1;
+        while (i < list.size()) {
+            pos = i;
+
+            if (list.get(i) == docId) {
+                return pos;
+            }
+            i++; //skip the occurrence
+            i++; // go to the next docid
+        }
+        return -1;
+    }
+
     @Override
     public int documentTermFrequency(String term, String url) {
         // Number of times {@code term} appeared in the document {@code url}
@@ -530,7 +546,7 @@ public class IndexerInvertedOccurrence extends Indexer {
         List<Integer> l = ivtGet(s.toString());
 
         // Use binary search looking for docid within given posting list.
-        int pos = binarySearchPostList(docid, l);
+        int pos = linearSearchPostList(docid, l);
 
         if (pos != -1) {
             // Return term frequency for given doc and term
