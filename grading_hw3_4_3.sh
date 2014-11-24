8973be2b635416a29527e3a0e2545ebf2e64351d
hui-yang <hy821@nyu.edu>
2014-11-24 11:10:03 -0500

    add stopwords; mod sh script

--- grading_hw3_4_3.sh
+++ grading_hw3_4_3.sh
@@ -3,7 +3,8 @@ i=0
 while read q ; do
 	i=$((i + 1));
 	prfout=prf-$i.tsv;
-	curl "http://localhost:25807/prf?query=$q&ranker=comprehensive&numdocs=10&numterms=5" > $prfout;
+	Q=`echo $q | sed -e "s/ /%20/g" -e "s/\"/%22/g"`
+	curl "http://localhost:25807/prf?query=$Q&ranker=comprehensive&numdocs=10&numterms=5" > $prfout;
 	echo $q:$prfout >> prf.tsv
 	done < queries.tsv
 java -cp src edu.nyu.cs.cs2580.Bhattacharyya prf.tsv qsim.tsv
